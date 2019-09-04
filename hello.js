280641f115147ebb20b3c4c7ffef78b9c1626b67
student <student@BTU.local>
2019-09-04 09:02:59 -0700

    add commands in hello.js

--- hello.js
+++ hello.js
@@ -1,2 +1,10 @@
 console.log("Hello Console")
 console.info("Information")
+
+
+
+console.log("test console")
+console.log("test console")
+console.log("test console")
+console.log("test console")
+console.log("test console")
