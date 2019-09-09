a3a1fed6494c39bdbc3aeaf8de81b1121c411502
student <student@BTU.local>
2019-09-09 08:15:11 -0700

    Start folder created

---
+++ Start/hello.js
@@ -0,0 +1,12 @@
+//console.log("Hello Console")
+//console.info("Information")
+
+/*
+
+console.log(7 + 10)
+console.log(7 - 10)
+console.log(7 * 10)
+console.log(7 / 10)
+
+
+*/
---
+++ Start/main.js
@@ -0,0 +1,3 @@
+console.log("test git commit ")
+
+document.write("<h1>Hello World. Javascript</h1>")
